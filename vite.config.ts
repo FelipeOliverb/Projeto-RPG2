eeb4a5bd170cf5284d2300d1fd30138ab93c8bc1
Felipe de Oliveira <felipe235711@gmail.com>
2026-02-11 17:53:05 -0300

    safdsgdfsdafsas

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
-  // base é usado apenas para build, não atrapalha o dev server
+  plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/Projeto-RPG2/" : "/",
 });
+
