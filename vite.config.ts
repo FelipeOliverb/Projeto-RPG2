affc0795b1c0bb3f33c0f7699ba6f39afe2e8b7b
Felipe de Oliveira <felipe235711@gmail.com>
2026-02-11 18:07:18 -0300

    sadafsas

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Projeto-RPG2/" : "/",
+  base: "/Projeto-RPG2/", // ESSENCIAL para GitHub Pages
 });
-
